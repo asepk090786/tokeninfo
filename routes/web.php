dd2b455ae49d94adb29d9d1f0803f7cdfc16d431
asepk090786 <asepk090786@users.noreply.github.com>
2026-04-02 05:53:33 +0000

    feat: tambah endpoint /api/endpoints sebagai agregator semua link API internal Exambro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CbtInfoController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CbtInfoController::class, 'index'])->name('cbt.index');
@@ -18,7 +19,78 @@ Route::match(['GET', 'OPTIONS'], '/api/exambro-status/peringatan', [CbtInfoContr
     ->middleware('exambro.key')
     ->name('cbt.exambro.status.warning');
 Route::get('/api/exambro-config/download', [CbtInfoController::class, 'downloadExambroApiConfigForApp'])
+    ->middleware('exambro.key')
     ->name('cbt.exambro.api-key.download.app');
+Route::get('/api/endpoints', function (Request $request) {
+    $providedKey = trim((string) $request->query('key', ''));
+
+    $buildUrl = function (string $routeName) use ($providedKey) {
+        return $providedKey !== ''
+            ? route($routeName, ['key' => $providedKey])
+            : route($routeName);
+    };
+
+    $endpoints = [
+        [
+            'name' => 'api_endpoints',
+            'method' => 'GET',
+            'path' => '/api/endpoints',
+            'url' => $buildUrl('cbt.api.endpoints'),
+            'requires_key' => false,
+        ],
+        [
+            'name' => 'token_info',
+            'method' => 'GET',
+            'path' => '/api/token-info',
+            'url' => $buildUrl('cbt.token.info'),
+            'requires_key' => false,
+        ],
+        [
+            'name' => 'exambro_info',
+            'method' => 'GET',
+            'path' => '/api/exambro-info',
+            'url' => $buildUrl('cbt.exambro.info'),
+            'requires_key' => true,
+        ],
+        [
+            'name' => 'exambro_status_token',
+            'method' => 'GET',
+            'path' => '/api/exambro-status/token',
+            'url' => $buildUrl('cbt.exambro.status.token'),
+            'requires_key' => true,
+        ],
+        [
+            'name' => 'exambro_status_warning',
+            'method' => 'GET',
+            'path' => '/api/exambro-status/peringatan',
+            'url' => $buildUrl('cbt.exambro.status.warning'),
+            'requires_key' => true,
+        ],
+        [
+            'name' => 'exambro_config_download',
+            'method' => 'GET',
+            'path' => '/api/exambro-config/download',
+            'url' => $buildUrl('cbt.exambro.api-key.download.app'),
+            'requires_key' => false,
+        ],
+    ];
+
+    return response()->json([
+        'message' => 'Daftar API internal untuk Exambro',
+        'count' => count($endpoints),
+        'base_url' => url('/'),
+        'auth' => [
+            'header' => 'X-Exambro-Key',
+            'query_param' => 'key',
+            'provided_key_included_in_url' => $providedKey !== '',
+        ],
+        'endpoints' => $endpoints,
+    ])->withHeaders([
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With, Accept, Origin, X-Exambro-Key',
+    ]);
+})->name('cbt.api.endpoints');
 
 Route::get('/admin/login', [CbtInfoController::class, 'showLogin'])->name('cbt.admin.login');
 Route::post('/admin/login', [CbtInfoController::class, 'login'])->name('cbt.admin.login.submit');
